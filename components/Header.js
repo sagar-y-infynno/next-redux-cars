658d1e3c7c9170145365691bdbdc36c8dada5bcf
Sagar Yadav M <108276720+sagar-y-infynno@users.noreply.github.com>
2022-09-05 18:16:40 +0530

    so icons added

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { FacebookShareButton ,WhatsappShareButton, InstapaperShareButton } from 'react-share';
+import { FacebookIcon, WhatsappIcon, InstapaperIcon } from 'react-share';
+
 
 export default function Header() {
   return (
@@ -9,6 +12,15 @@ export default function Header() {
         <Link href="/" ><a  className="header-link">HOW IT WORKS</a></Link>
         <Link href="/" ><a className="header-link">WHY US</a></Link>
         <Link href="/" ><a className="header-link">CONTACT US</a></Link>
+        <FacebookShareButton quote='this is autodigg' hashtag='#autodigg' url="http://localhost:3000/" >
+          <FacebookIcon size={32} round={true} />
+        </FacebookShareButton>
+        <WhatsappShareButton url="http://localhost:3000/" >
+          <WhatsappIcon size={32} round={true} />
+        </WhatsappShareButton>
+        <InstapaperShareButton description='this is autodigg a new and used car buying site' title=""  url="http://localhost:3000/" >
+          <InstapaperIcon size={32} round={true} />
+        </InstapaperShareButton>
       </div>
       <Link href="/" >
         <a>
